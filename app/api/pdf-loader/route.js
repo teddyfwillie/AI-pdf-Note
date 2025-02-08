53cf1516da2b33e1cd89747eb434328f2ca20381
teddyfwillie <willieteddyf@gmail.com>
2025-02-09 02:35:24 +0530

    Added PDF Loader & Text Splitter

---
+++ app/api/pdf-loader/route.js
@@ -0,0 +1,34 @@
+import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { NextResponse } from "next/server";
+
+const pdfUrl =
+  "https://adept-marmot-953.convex.cloud/api/storage/feb5af36-65d9-407a-bf35-b72a2e075f6d";
+
+export async function GET(req) {
+  // 1. Fetch the PDF file
+  const response = await fetch(pdfUrl);
+  const data = await response.blob();
+  const loaser = new WebPDFLoader(data);
+  const docs = await loaser.load();
+
+  let pdfTextContent = "";
+  docs.forEach((doc) => {
+    pdfTextContent = pdfTextContent + doc.pageContent;
+  });
+
+  // 2. split the text content into words
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 100,
+    chunkOverlap: 0,
+  });
+
+  const output = await textSplitter.splitText(pdfTextContent);
+
+  let splitterList = [];
+  output.forEach((doc) => {
+    splitterList.push(doc.pageContent);
+  });
+
+  return NextResponse.json({ result: pdfTextContent });
+}
